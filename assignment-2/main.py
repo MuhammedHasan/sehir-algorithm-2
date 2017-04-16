ac8ab5ec74df03ef26dc7897f326ebd80aa672f1
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-04-16 19:14:20 +0300

    assignment 2

--- assignment-2/main.py
+++ assignment-2/main.py
@@ -1,3 +1,29 @@
+import heapq
+
+
+class PriorityQueue(list):
+
+    def __init__(self, inlist=None):
+        super(PriorityQueue, self).__init__(inlist or [])
+        heapq.heapify(self)
+
+    def insert(self, item):
+        heapq.heappush(self, item)
+
+    def peek(self):
+        return self[0]
+
+    def pop(self):
+        return heapq.heappop(self)
+
+    def decrease_key(self, item):
+        for i, v in enumerate(self):
+            if v[1] == item[1]:
+                self[i] = item
+                break
+        heapq.heapify(self)
+
+
 class Grid:
 
     def __init__(self, grid_list):
@@ -8,21 +34,32 @@ class Grid:
             for j, cell in enumerate(row):
                 yield (i, j, cell)
 
-    def neighbors(self, i, j):
-        for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+    def neighbors(self, i, j, directions="all"):
+        if directions == 'all':
+            directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        elif directions == 'right-down':
+            directions = [(0, 1), (1, 0)]
+        else:
+            raise ValueError('Directions are not valid')
+        for x, y in directions:
             nc = (i + x, j + y)
             if self.in_grid(nc[0], nc[1]):
-                ncell = self._grid[nc[0], nc[1]]
+                ncell = self._grid[nc[0]][nc[1]]
                 if ncell == 'X':
                     nnc = (i + 2 * x, j + 2 * y)
                     if self.in_grid(nnc[0], nnc[1]):
-                        nncell = self._grid[nnc[0], nnc[1]]
+                        nncell = self._grid[nnc[0]][nnc[1]]
                         if nncell != 'X':
                             yield (nnc[0], nnc[1], 2 * nncell)
-                    yield (nc[0], nc[1], ncell)
+                    continue
+                yield (nc[0], nc[1], ncell)
 
     def in_grid(self, i, j):
-        return len(self._grid) > i > 0 and len(self._grid[0]) > j > 0
+        return self.size[0] > i >= 0 and self.size[1] > j >= 0
+
+    @property
+    def size(self):
+        return (len(self._grid), len(self._grid[0]))
 
 
 class Graph(dict):
@@ -33,29 +70,69 @@ class Graph(dict):
     def add_edge(self, node1, node2, w1, w2=0, directed=False):
         [self.add_node(n) for n in [node1, node2] if n not in self]
         self[node1][node2] = w1
-        if not directed:
-            self[node2][node1] = w2
-
-    def topological_order(self):
-        pass
 
     @staticmethod
-    def from_grid(grid_list):
+    def from_grid(grid_list, directions='all'):
         graph, grid = Graph(), Grid(grid_list)
+        graph._size = grid.size
         for i, j, v in grid.cells():
-            for ni, nj, nv in grid.neighbors(i, j):
-                graph.add_edge((i, j), (ni, nj), nv, v)
+            if v is not 'X':
+                for ni, nj, nv in grid.neighbors(i, j, directions):
+                    graph.add_edge((i, j), (ni, nj), nv, v)
         return graph
 
+    def init_prev_dists(self, node):
+        prev = dict()
+        dists = {n: float('inf') for n in self}
+        dists[node] = 0
+        return prev, dists
+
+    def dijkstra(self, node):
+        prev, dists = self.init_prev_dists(node)
+        pq = PriorityQueue([(w, n) for n, w in dists.items()])
+
+        while pq:
+            (dist, n) = pq.pop()
+            for nn, nw in self[n].items():
+                if dists[nn] > dist + nw:
+                    dists[nn] = dist + nw
+                    pq.decrease_key((dists[nn], nn))
+                    prev[nn] = n
+        return dists, prev
+
+    def prev_to_path(self, prev, node):
+        paths = [node]
+        while paths[-1] in prev:
+            paths.append(prev[paths[-1]])
+        return paths
+
+    def find_sortest_path(self, from_node, to_node):
+        dists, prev = self.dijkstra(from_node)
+        paths = self.prev_to_path(prev, to_node)
+        return dists[to_node], list(reversed(paths))
+
+    def topological_order(self):
+        for i in range(self._size[0]):
+            for j in range(self._size[1]):
+                if (i, j) in self:
+                    yield (i, j)
+
+    def find_sortest_path_for_neg(self, from_node, to_node):
+        prev, dists = self.init_prev_dists(from_node)
+
+        for n in self.topological_order():
+            for nn, nw in self[n].items():
+                if dists[nn] > dists[n] + nw:
+                    dists[nn] = dists[n] + nw
+                    prev[nn] = n
+
+        paths = self.prev_to_path(prev, to_node)
+        return dists[to_node], list(reversed(paths))
+
     @staticmethod
-    def from_negative_graph(grid):
-        pass
+    def from_negative_graph(grid_list):
+        return Graph.from_grid(grid_list, directions='right-down')
 
     def add_check_point(self):
         # think about cost
         pass
-
-    def find_sortest_path(self):
-        for n in self.topological_order():
-            for nn in self[n]:
-                pass
--- assignment-2/tests.py
+++ assignment-2/tests.py
@@ -1,15 +1,68 @@
 import unittest
-from main import Graph, Grid
+from main import Graph, Grid, PriorityQueue
 
 
-class TestGraph(unittest.TestCase):
+class PriorityQueueTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.G = [[0, 'X', 1, 4, 9, 'X'],
-                  [7, 7, 4, 'X', 4, 8],
-                  [3, 'X', 3, 2, 'X', 4],
-                  [10, 2, 5, 'X', 3, 0]]
-        self.graph = Graph()
+        self.pq = PriorityQueue([(1, 'a'), (4, 'd'), (3, 'c'), (5, 'b')])
+
+    def test_init(self):
+        self.assertEqual(PriorityQueue(), [])
+        self.assertEqual(self.pq, [(1, 'a'), (4, 'd'), (3, 'c'), (5, 'b')])
+
+    def test_pop(self):
+        self.assertEqual(self.pq.pop(), (1, 'a'))
+        self.assertEqual(self.pq.pop(), (3, 'c'))
+
+    def test_peek(self):
+        self.assertEqual(self.pq.peek(), (1, 'a'))
+
+    def test_insert(self):
+        self.pq.insert((0, 'x'))
+        self.assertEqual(self.pq.peek(), (0, 'x'))
+
+    def test_decrease_key(self):
+        self.pq.decrease_key((6, 'a'))
+        self.assertEqual(self.pq, [(3, 'c'), (4, 'd'), (6, 'a'), (5, 'b')])
+
+
+class GridTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.G = [[0, 'X', 1,  4,  9, 'X'],
+                  [7,  7,  4, 'X', 4,  8],
+                  [3, 'X', 3,  2, 'X', 4],
+                  [10, 2,  5, 'X', 3,  0]]
+        self.grid = Grid(self.G)
+
+    def test_cells(self):
+        size = len(self.G) * len(self.G[0])
+        self.assertEqual(len(list(self.grid.cells())), size)
+
+    def test_neighbors(self):
+        self.assertEqual(set(self.grid.neighbors(0, 0)),
+                         set([(0, 2, 2), (1, 0, 7)]))
+
+        self.assertEqual(set(self.grid.neighbors(1, 1)),
+                         set([(1, 2, 4), (3, 1, 4), (1, 0, 7)]))
+
+
+class GraphTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.G = [[0, 'X', 1,  4,  9, 'X'],
+                  [7,  7,  4, 'X', 4,  8],
+                  [3, 'X', 3,  2, 'X', 4],
+                  [10, 2,  5, 'X', 3,  0]]
+
+        self.G_neg = [[0, 'X', 5, -3,  6, 'X'],
+                      [6, -4,  5, 'X', 3, -8],
+                      [4, 'X', 3, -7, 'X', 5],
+                      [10, 2, -2, 'X', 6,  0]]
+
+        self.graph = Graph.from_grid(self.G)
+        self.graph_neg = Graph.from_negative_graph(self.G_neg)
 
     def test_add_node(self):
         node = (1, 2)
@@ -17,23 +70,43 @@ class TestGraph(unittest.TestCase):
         self.assertTrue(node in self.graph)
 
     def test_add_edge(self):
-        pass
+        self.graph.add_edge((1, 1), (2, 2), 1, 2)
+        self.assertEqual(self.graph[1, 1][2, 2], 1)
 
     def test_topological_order(self):
         pass
 
     def test_from_grid(self):
-        pass
+        self.assertEqual(self.graph[1, 2][2, 2], 3)
+        self.assertEqual(self.graph[2, 2][1, 2], 4)
+        self.assertEqual(self.graph[1, 2][1, 4], 8)
+        self.assertEqual(self.graph[1, 4][1, 2], 8)
+
+    def test_find_sortest_path(self):
+        to_node = len(self.G) - 1, len(self.G[0]) - 1
+        from_node = (0, 0)
+        dist, path = self.graph.find_sortest_path(from_node, to_node)
+        self.assertEqual(dist, 18)
+        self.assertEqual(path,
+                         [(0, 0), (0, 2), (0, 3), (2, 3), (2, 5), (3, 5)])
 
     def test_from_negative_graph(self):
-        pass
+        neg_graph = Graph.from_negative_graph(self.G)
+        self.assertEqual(neg_graph[1, 2][2, 2], 3)
+
+    def test_find_sortest_path_for_neg(self):
+        to_node = len(self.G) - 1, len(self.G[0]) - 1
+        from_node = (0, 0)
+        dist, path = self.graph_neg.find_sortest_path_for_neg(
+            from_node, to_node)
+        self.assertEqual(dist, 3)
+        self.assertEqual(path,
+                         [(0, 0), (0, 2), (0, 3), (2, 3), (2, 5), (3, 5)])
 
     def test_add_check_point(self):
         # think about cost
         pass
 
-    def test_find_sortest_path(self):
-        pass
 
 if __name__ == '__main__':
     unittest.main()
